0434dd08ba00641664653a209139b6e8e9f726a8
YordinCR <yycruzr@unitru.edu.pe>
2026-04-07 21:30:45 -0500

    Anadi al cucu

---
+++ src/Cucu/Ferrer.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Cucu;
+
+/**
+ *
+ * @author yordi
+ */
+public class Ferrer {
+    
+    public static void main(String[] args) {
+        
+        System.out.println("Ya fue");
+        
+    }
+    
+}
